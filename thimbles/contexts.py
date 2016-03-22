09f86737e16136db71051bec31c321dcea969d2b
Tim Anderton <quidditymaster@gmail.com>
2016-03-22 10:50:48 -0600

    updated contextualization engines to use query factories which generate directly from python expressions

--- thimbles/contexts.py
+++ thimbles/contexts.py
@@ -8,62 +8,74 @@ from thimbles.spectrographs import Aperture, Chip, Order
 from thimbles.observations import Exposure
 from thimbles.spectrum import Spectrum
 
+class PyEvalQuery(object):
+    """
+A convenience class for generating queries to databases from 
+python strings.
+"""
+    _global_ns = wds.__dict__
+    
+    def __init__(self, query_expression):
+        self.query_expression = query_expression
+    
+    def __call__(self, db):
+        return eval(self.query_expression, self._global_ns, {"db":db})
+
+
+class PyEvalFilter(object):
+    _global_ns = wds.__dict__
+    
+    def __init__(self, filter_expression):
+        self.filter_epression = filter_expression
+    
+    def __call__(self, results):
+        filter_func = eval(self.filter_expression, self._global_ns)
+        return filter_func(results)
+
+
+class BlockPartitionTagFilter(object):
+    
+    def __init__(self, block_size):
+        self.block_size = block_size
+    
+    def __call__(self, query, tag):
+        offset = tag*self.block_size
+        limit = (tag+1)*self.block_size
+        return q.offset(offset).limit(limit)
+
+
 class ContextualizationEngine(object):
     cregistry = None
-    _query_db = None
-    _query = None
+    _query_global_ns = wds.__dict__
     
     def __init__(
             self,
-            table_spec,
+            query_factory,
             extractors=None,
-            join_spec=None,
-            filter_factory=None,
             tag_filter_factory=None,
-            post_query_filter=None,
-            offset=None,
-            limit=100,
+            python_filter=None,
     ):
-        self.table_spec = table_spec
-        self.filter_factory = filter_factory
-        self.post_query_filter=post_query_filter
-        self.join_spec = join_spec
-        self.offset = offset
-        self.limit = limit
+        self.query_factory = query_factory
+        self.python_filter = python_filter
+        self.tag_filter_factory = tag_filter_factory
         if extractors is None:
             extractors = {}
         self.extractors = extractors
     
-    def add_named_context(self, name, extractor):
-        self.extractors[name] = extractor
-    
-    def pre_query(self, db=None):
-        if db is None:
-            db = wds.db
-        if self._query_db is db:
-            if not self._query is None:
-                return self._query
-        q = db.query(*self.table_spec)
-        if not self.join_spec is None:
-            q = q.join(self.join_spec)
-        if not self.filter_factory is None:
-            q = self.filter_factory(q)
-        if not self.offset is None:
-            q=q.offset(self.offset)
-        if not self.limit is None:
-            q=q.limit(self.limit)
-        return q
-    
     def find(self, tag=None, db=None):
-        q = self.pre_query(db=db)
-        if not (tag is None):
+        q = self.query_factory(db=db)
+        if (not self.tag_filter_factory is None) and (not (tag is None)):
             q = self.tag_filter_factory(q, tag)
         #actually emit the query
         results = q.all()
-        #do a post query python cleanup 
-        if not self.post_query_filter is None:
-            results = self.post_query_filter(results)
+        
+        #do a post sql query python cleanup 
+        if not self.python_filter is None:
+            results = self.python_filter(results)
         return results
+    
+    def add_named_context(self, name, extractor):
+        self.extractors[name] = extractor
     
     def contextualize(self, result):
         if not self.cregistry is None:
@@ -71,9 +83,9 @@ class ContextualizationEngine(object):
         else:
             cdict = {}
         for cname in self.extractors:
-            cdict[cname] = self.extractors[cname](instance)
+            cdict[cname] = self.extractors[cname](result)
         return cdict
-
+    
     def set_context_registry(self, reg):
         self.cregistry = reg
 
@@ -96,71 +108,71 @@ class ContextualizationRegistry(object):
         self.spines[index] = value
 
 
-contextualizers = ContextualizationRegistry()
-global_ce = ContextualizationEngine(
-    table_spec=[tmb.analysis.SharedParameterSpace],
-    filter_factory=lambda q: q.filter(tmb.analysis.SharedParameterSpace.name == "global"),
+model_spines = ContextualizationRegistry()
+global_spine = ContextualizationEngine(
+    PyEvalQuery("db.query(SharedParameterSpace).filter(SharedParameterSpace.name == 'global')"),
 )
-contextualizers["global"] = global_ce
+model_spines["global"] = global_spine
 
-star_contextualizer = ContextualizationEngine(
-    table_spec = [Star],
+
+star_spine = ContextualizationEngine(
+    PyEvalQuery("db.query(Star)"),
     tag_filter_factory = lambda query, name : query.filter(Star.name == name),
     extractors={
         "star": lambda x: x,
     },
 )
-contextualizers["stars"] = star_contextualizer
+model_spines["stars"] = star_spine
 
 SourceGrouping = tmb.sources.SourceGrouping
-grouping_contextualizer = ContextualizationEngine(
-    table_spec=[SourceGrouping],
+grouping_spine = ContextualizationEngine(
+    PyEvalQuery("db.query(SourceGrouping)"),
     tag_filter_factory=lambda query, name: query.filter(SourceGrouping.name == name),
     extractors = {
         "group": lambda x: x
     },
 )
-contextualizers["source_groupings"] = grouping_contextualizer
+model_spines["source_groupings"] = grouping_spine
 
-aperture_contextualizer = ContextualizationEngine(
-    table_spec = Aperture,
+aperture_spine = ContextualizationEngine(
+    PyEvalQuery("db.query(Aperture)"),
     tag_filter_factory = lambda query, name : query.filter(tmb.spectrographs.Aperture.name == name),
     extractors={
         "aperture": lambda x:x,
     }
 )
-contextualizers["apertures"] = aperture_contextualizer
+model_spines["apertures"] = aperture_spine
 
-order_contextualizer = ContextualizationEngine(
-    table_spec=[tmb.spectrographs.Order],
+order_spine = ContextualizationEngine(
+    PyEvalQuery("db.query(Order)"),
     tag_filter_factory = lambda query, number : query.filter(Order.number == number),
     extractors={
         "order": lambda x: x
     }
 )
-contextualizers["orders"] = order_contextualizer
+model_spines["orders"] = order_spine
 
-chip_contextualizer = ContextualizationEngine(
-    table_spec = [Order],
+chip_spine = ContextualizationEngine(
+    PyEvalQuery("db.query(Chip)"),
     tag_filter_factory = lambda query, name : query.filter(Chip.name == name),
     extractors = {
         "chip": lambda x: x,
     }
 )
-contextualizers["chips"] = chip_contextualizer
+model_spines["chips"] = chip_spine
 
-exposure_contextualizer = ContextualizationEngine(
-    table_spec = [Exposure],
+exposure_spine = ContextualizationEngine(
+    PyEvalQuery("db.query(Exposure)"),
     tag_filter_factory = lambda query, name : query.filter(Exposure.name == name),
     extractors = {
         "exposure": lambda x: x,
     }
 )
-contextualizers["exposures"] = exposure_contextualizer
+model_spines["exposures"] = exposure_spine
 
 
-spectrum_contextualizer = ContextualizationEngine(
-    table_spec = [Spectrum],
+spectrum_spine = ContextualizationEngine(
+    PyEvalQuery("db.query(Spectrum)"),
     extractors = {
         "spectrum" : lambda x: x,
         "source" : lambda x: x.source,
@@ -169,11 +181,11 @@ spectrum_contextualizer = ContextualizationEngine(
         "chip": lambda x: x.chip
     }
 )
-contextualizers["spectra"] = spectrum_contextualizer
+model_spines["spectra"] = spectrum_spine
 
 
 source_spectra_pairs = ContextualizationEngine(
-    table_spec = [Spectrum, Source],
+    PyEvalQuery("db.query(Spectrum, Source).filter(Spectrum._source_id == Source._id)"),
     extractors = {
         "spectrum" : lambda x: x[0],
         "source" : lambda x: x[1],
@@ -182,5 +194,5 @@ source_spectra_pairs = ContextualizationEngine(
         "chip": lambda x: x[0].chip
     }
 )
-contextualizers["source spectrum pairs"] = source_spectra_pairs
+model_spines["source spectrum pairs"] = source_spectra_pairs
 

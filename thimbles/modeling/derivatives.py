fixed path derivative matrix shape bug removed explicit Estimator database presence

thimbles/modeling/derivatives.py
@@ -1,6 +1,8 @@
 import numpy as np
+import scipy
 import scipy.sparse as sparse
 
+import thimbles as tmb
 from .paths import execute_path, extract_influence_graph, find_all_paths
 
 
@@ -11,37 +13,40 @@ class BrokenDerivativeChain(Exception):
     pass
 
 
-def deriv(response_params, input_params, combine_matrices=True):
+def deriv(response_params, input_params, combine_matrices=True, **deriv_kwargs):
     nrow = len(response_params)
     ncol = len(input_params)
-    blocks = [[None for j in range(nrow)] for i in range(ncol)]
+    blocks = [[None for j in range(ncol)] for i in range(nrow)]
     
     influence_graph = extract_influence_graph(response_params, direction="upstream")
     for rp_idx, rp in enumerate(response_params):
         for ip_idx, ip in enumerate(input_params):
             connecting_paths = find_all_paths(ip, rp, influence_graph=influence_graph)
             for path in connecting_paths:
-                cder_mat = path_deriv(path)
+                cder_mat = path_deriv(path, **deriv_kwargs)
                 prev_der = blocks[rp_idx][ip_idx]
                 if prev_der is None:
                     blocks[rp_idx][ip_idx] = cder_mat
                 else:
                     blocks[rp_idx][ip_idx] = prev_der + cder_mat
     if combine_matrices:
-        dermat = scipy.sparse.bmat(blocks)
+        if (len(response_params) == 1) and (len(input_params)==1):
+            dermat = blocks[0][0]
+        else:
+            dermat = scipy.sparse.bmat(blocks)
     else:
         dermat = blocks
     return dermat
 
 
-def path_deriv(path, max_discrete_n=50, fallback_epsilon=1e-4):
+def path_deriv(path, max_discrete_n=50, fallback_epsilon=1e-3):
     assert len(path) >= 3
     assert len(path) % 2 == 1
     params = path[::2]
     mods = path[1::2]
     
-    assert all([isinstance(p, Parameter) for p in params])
-    assert all([isinstance(m, Model) for m in mods])
+    assert all([isinstance(p, tmb.modeling.Parameter) for p in params])
+    assert all([isinstance(m, tmb.modeling.Model) for m in mods])
     #try and use the fast_deriv model methods
     try:
         tot_deriv = mods[0].fast_deriv(params[0])
@@ -55,24 +60,35 @@ def path_deriv(path, max_discrete_n=50, fallback_epsilon=1e-4):
         return tot_deriv
     except BrokenDerivativeChain:
         cur_input_val = path[0].value
+
+        if hasattr(cur_input_val, "shape"):
+            base_shape = cur_input_val.shape
+        else:
+            base_shape = None
         
         central_input_val = np.atleast_1d(cur_input_val)
-        ndim_input = len(cur_input_val)
+        ndim_input = len(central_input_val)
         if ndim_input > max_discrete_n:
             raise MaxDerivativeComplexityExceeded()
-        
-        der_cols = []
-        for dim_idx in range(ndim_input):
-            delta_vec = central_input_val.copy()
 
-            delta_vec[dim_idx] = central_input_val[dim_idx]+fallback_epsilon
-            plus_d = execute_path(path, root_value=delta_vec)
+        if not (base_shape is None):
+            der_cols = []
+            for dim_idx in range(ndim_input):
+                delta_vec = central_input_val.copy()
 
-            delta_vec[dim_idx] = central_input_val[dim_idx]-fallback_epsilon
-            neg_d  = execute_path(path, root_value=delta_vec)
+                delta_vec[dim_idx] = central_input_val[dim_idx]+fallback_epsilon
+                plus_d = execute_path(path, root_value=delta_vec)
+                
+                delta_vec[dim_idx] = central_input_val[dim_idx]-fallback_epsilon
+                neg_d  = execute_path(path, root_value=delta_vec)
             
+                dcol = (plus_d-neg_d)/(2.0*fallback_epsilon)
+                der_cols.append(scipy.sparse.csc_matrix(dcol.reshape((-1, 1))))
+            pder_mat = scipy.sparse.hstack(der_cols)
+            return pder_mat
+        else:
+            plus_d = execute_path(path, root_value=cur_input_val + fallback_epsilon)
+            neg_d = execute_path(path, root_value=cur_input_val - fallback_epsilon)
             dcol = (plus_d-neg_d)/(2.0*fallback_epsilon)
-            der_cols.append(scipy.sparse.csc_matrix(deriv.reshape((-1, 1))))
-        pder_mat = scipy.sparse.hstack(der_cols)
-        return pder_mat
+            return scipy.sparse.csc_matrix(dcol.reshape((-1, 1)))
 

thimbles/modeling/factor_models.py
@@ -6,6 +6,7 @@ from thimbles.modeling.models import Model, Parameter
 from thimbles.thimblesdb import HasName
 from thimbles.sqlaimports import *
 from functools import reduce
+import scipy
 
 __all__ = \
 """
@@ -59,7 +60,9 @@ class MultiplierModel(Model):
                 param_is_dependent = True
         if not param_is_dependent:
             raise ValueError("given parameter is not an input of this model!")
-        return complimentary_prod
+        out_shape = (len(complimentary_prod), len(complimentary_prod))
+        der_mat = scipy.sparse.dia_matrix((complimentary_prod, 0), shape=out_shape)
+        return der_mat
 
 
 class PickleParameter(Parameter):
@@ -103,8 +106,8 @@ class MatrixMultiplierModel(Model):
     
     def __init__(self, output_p, matrix, vector):
         self.output_p = output_p
-        self.add_parameter("matrix", matrix_p)
-        self.add_parameter("vector", vector_p)
+        self.add_parameter("matrix", matrix)
+        self.add_parameter("vector", vector)
     
     def __call__(self, vprep=None):
         vdict = self.get_vdict(vprep)